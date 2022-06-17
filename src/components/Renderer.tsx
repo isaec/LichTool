52477ad8a8a6c4873a6e72b4be7774c05f5d5e65
Isaac <isaac@rubenfamily.com>
2022-06-17 00:55:45 -0700

    format, organize

--- src/components/Renderer.tsx
+++ src/components/Renderer.tsx
@@ -1,65 +1,85 @@
-import { Component, For } from "solid-js"
+import { Component, For } from "solid-js";
 
-import styles from "./Renderer.module.scss"
+import styles from "./Renderer.module.scss";
 
-type DataNode = { [key: string]: Data }
-type DataGroup = DataNode[]
-type Data = string | DataNode | DataGroup
+type DataNode = { [key: string]: Data };
+type DataGroup = DataNode[];
+type Data = string | DataNode | DataGroup;
 
-type DataNodeType = Component<{data: DataNode}>
+type DataNodeType = Component<{ data: DataNode }>;
 
-const SectionType: DataNodeType = props => {
-  return <><h1>{props.data.name as string}</h1><DataGroupRenderer group={props.data.entries as DataGroup}/></>
-}
+const entryTypes = new Map<string, DataNodeType>(
+  Object.entries({
+    // each key corresponds to a "type": "xxx"
+    // each value is jsx to render that type
+    section: (props) => (
+      <>
+        <h1>{props.data.name as string}</h1>
+        <DataGroupRenderer group={props.data.entries as DataGroup} />
+      </>
+    ),
+    list: (props) => (
+      <ul>
+        <For each={props.data.items as Data[]}>
+          {(item) => <li>{<DataRenderer data={item} />}</li>}
+        </For>
+      </ul>
+    ),
+  })
+);
 
-const ListType: DataNodeType = props => {
-  return <ul><For each={props.data.items as Data[]}>{(item) => <li>{<DataRenderer data={item} />}</li>}</For></ul>
-}
+const DataStringRenderer: Component<{ string: string }> = (props) => {
+  console.log("string renderer >>", props.string);
+  return (
+    <p class={styles.string}>
+      {"string: "}
+      {props.string}
+    </p>
+  );
+};
 
-const entryTypes = new Map<string, DataNodeType>(Object.entries({
-  "section": SectionType,
-  "list": ListType
-}))
+const DataGroupRenderer: Component<{ group: DataGroup }> = (props) => {
+  console.log("group renderer >>", props.group);
+  return (
+    <p>
+      {"group: "}
+      <For each={props.group}>{(data) => <DataRenderer data={data} />}</For>
+    </p>
+  );
+};
 
-const DataStringRenderer: Component<{string: string}> = props => {
-  console.log("string renderer >>", props.string)
-  return <p class={styles.string} >{"string: "}{props.string}</p>
-}
+const DataNodeRenderer: Component<{ data: DataNode }> = (props) => {
+  console.log("node renderer >>", props.data);
 
-const DataGroupRenderer: Component<{group: DataGroup}> = props => {
-  console.log("group renderer >>", props.group)
-  return <p>{"group: "}<For each={props.group}>{(data) => <DataRenderer data={data}/>}</For></p>
-}
+  const Entry = entryTypes.get(props.data.type as string);
 
-const DataNodeRenderer: Component<{data: DataNode}> = props => {
-  console.log("node renderer >>", props.data)
-
-  const Entry = entryTypes.get(props.data.type as string)
-
-  if(Entry === undefined) {
-    return <p class={styles.error} >{"data (UNKNOWN TYPE FAILURE): "}{JSON.stringify(props.data)}</p>
+  if (Entry === undefined) {
+    return (
+      <p class={styles.error}>
+        {"data (UNKNOWN TYPE FAILURE): "}
+        {JSON.stringify(props.data)}
+      </p>
+    );
   }
 
-  return <Entry data={props.data}/>
-}
+  return <Entry data={props.data} />;
+};
 
-const DataRenderer: Component<{data: Data}> = props => {
-
-  if(typeof props.data === "string") {
-    return <DataStringRenderer string={props.data} />
+const DataRenderer: Component<{ data: Data }> = (props) => {
+  if (typeof props.data === "string") {
+    return <DataStringRenderer string={props.data} />;
   }
 
-  if(Array.isArray(props.data)) {
-    return <DataGroupRenderer group={props.data} />
+  if (Array.isArray(props.data)) {
+    return <DataGroupRenderer group={props.data} />;
   }
-  
-  return <DataNodeRenderer data={props.data} />
-}
 
-const Renderer: Component<{data: string}> = (props) => {
+  return <DataNodeRenderer data={props.data} />;
+};
 
-  const dataObj: Data = JSON.parse(props.data)
+const Renderer: Component<{ data: string }> = (props) => {
+  const dataObj: Data = JSON.parse(props.data);
 
-  return <DataRenderer data={dataObj} />
-}
-export default Renderer
+  return <DataRenderer data={dataObj} />;
+};
+export default Renderer;
