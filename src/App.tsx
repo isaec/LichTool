3e3bdfc7cc774838ea7acf54c26c134cf560e21a
Isaac <isaac@rubenfamily.com>
2022-06-16 22:13:03 -0700

    import renderdemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Component, createSignal } from 'solid-js';
+// @ts-ignore
+import renderdemo from "./data/renderdemo.json"
 
 import styles from './App.module.scss';
 import Renderer from './components/Renderer';
+
+console.log(renderdemo)
 
 const App: Component = () => {
 
