89e9bcb6138133f5215dff01d28d917d1bb2d721
Isaac <isaac@rubenfamily.com>
2022-06-17 03:05:31 -0700

    return to using map

--- src/components/Renderer.tsx
+++ src/components/Renderer.tsx
@@ -6,35 +6,35 @@ type SectionData = { type: "section"; name: string; entries: DataGroup };
 type ListData = { type: "list"; items: Data[] };
 type InsetData = { type: "inset"; name: string; entries: Data };
 
-type DataTypes = SectionData | ListData | InsetData;
-
-type DataNode = DataTypes;
+type DataNode = SectionData | ListData | InsetData;
 type DataGroup = DataNode[];
 type Data = string | DataNode | DataGroup;
 
-const entryTypes = {
-  // each key corresponds to a "type": "xxx"
-  // each value is jsx to render that type
-  section: (props: { data: SectionData }) => (
-    <>
-      <h1>{props.data.name}</h1>
-      <DataGroupRenderer group={props.data.entries} />
-    </>
-  ),
-  list: (props: { data: ListData }) => (
-    <ul>
-      <For each={props.data.items as Data[]}>
-        {(item) => <li>{<DataRenderer data={item} />}</li>}
-      </For>
-    </ul>
-  ),
-  inset: (props: { data: InsetData }) => (
-    <div class={styles.inset}>
-      <h1>{props.data.name as string}</h1>
-      {<DataRenderer data={props.data.entries} />}
-    </div>
-  ),
-};
+const entryTypes = new Map(
+  Object.entries({
+    // each key corresponds to a "type": "xxx"
+    // each value is jsx to render that type
+    section: (props: { data: SectionData }) => (
+      <>
+        <h1>{props.data.name}</h1>
+        <DataGroupRenderer group={props.data.entries} />
+      </>
+    ),
+    list: (props: { data: ListData }) => (
+      <ul>
+        <For each={props.data.items}>
+          {(item) => <li>{<DataRenderer data={item} />}</li>}
+        </For>
+      </ul>
+    ),
+    inset: (props: { data: InsetData }) => (
+      <div class={styles.inset}>
+        <h1>{props.data.name}</h1>
+        {<DataRenderer data={props.data.entries} />}
+      </div>
+    ),
+  })
+);
 
 const DataStringRenderer: Component<{ string: string }> = (props) => {
   console.log("string renderer >>", props.string);
@@ -59,7 +59,7 @@ const DataGroupRenderer: Component<{ group: DataGroup }> = (props) => {
 const DataNodeRenderer: Component<{ data: DataNode }> = (props) => {
   console.log("node renderer >>", props.data);
 
-  const Entry = entryTypes[props.data.type];
+  const Entry = entryTypes.get(props.data.type);
 
   if (Entry === undefined) {
     return (
@@ -70,7 +70,13 @@ const DataNodeRenderer: Component<{ data: DataNode }> = (props) => {
     );
   }
 
-  return <Entry data={props.data as keyof typeof Entry} />;
+  return (
+    <Entry
+      data={
+        props.data as keyof typeof Entry /* this is safe because we checked the json type - ts just cant tell */
+      }
+    />
+  );
 };
 
 const DataRenderer: Component<{ data: Data }> = (props) => {
