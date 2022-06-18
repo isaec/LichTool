a746c51c8cd2086f8f9e602d6e658713a48652ba
Isaac <isaac@rubenfamily.com>
2022-06-17 22:54:20 -0700

    show path of patched file

--- fix-jest-dom.mjs
+++ fix-jest-dom.mjs
@@ -14,6 +14,6 @@ fs.readFile(typesPath, "utf8", (err, data) => {
 
   fs.writeFile(typesPath, data.replace(refMatcher, ""), "utf8", function (err) {
     if (err) throw err;
-    console.log("patched @testing-library/jest-dom 🎉");
+    console.log("patched @testing-library/jest-dom 🎉\npath:", typesPath);
   });
 });
