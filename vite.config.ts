7c4f02724c5cdf79b2f8ef51425a0a5760a4eb98
Isaac <isaac@rubenfamily.com>
2022-06-17 21:04:29 -0700

    start work on setting up vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfigExport } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    transformMode: {
+      web: [/\.[jt]sx?$/],
+    },
+    setupFiles: './setupVitest.ts',
+    // solid needs to be inline to work around
+    // a resolution issue in vitest:
+    deps: {
+      inline: [/solid-js/],
+    },
+    // if you have few tests, try commenting one
+    // or both out to improve performance:
+    threads: false,
+    isolate: false,
+  },
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
   },
-});
+  resolve: {
+    conditions: ['development', 'browser'],
+  }
+} as UserConfigExport);
