435a0c1f629c812bd14bb2188cc83507642dc675
Isaac <isaac@rubenfamily.com>
2022-06-17 23:23:22 -0700

    working unit tests

--- src/components/Renderer.test.tsx
+++ src/components/Renderer.test.tsx
@@ -1,13 +1,16 @@
 import { describe, expect, it } from "vitest";
 import { render, fireEvent } from "solid-testing-library";
-import dataJson from "../data/renderdemo.json?raw";
+import renderdemo from "../data/renderdemo.json";
 
 import Renderer from "./Renderer";
 
 describe("Renderer", () => {
   it("will render without crashing", () => {
-    const { unmount, getByText } = render(() => <Renderer data={dataJson} />);
-    expect(getByText("fireball")).toBeInTheDocument();
+    const { unmount, getByText } = render(() => (
+      <Renderer data={JSON.stringify(renderdemo.data[0])} />
+    ));
+    expect(getByText("Renderer Demo")).toBeInTheDocument();
+
     unmount();
   });
 });
