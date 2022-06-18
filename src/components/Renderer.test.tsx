4f3b933f74d214425ed4c3aa7c453dcbea810493
Isaac <isaac@rubenfamily.com>
2022-06-18 10:48:18 -0700

    snapshot generated html

--- src/components/Renderer.test.tsx
+++ src/components/Renderer.test.tsx
@@ -1,16 +1,23 @@
 import { describe, expect, it } from "vitest";
 import { render, fireEvent } from "solid-testing-library";
+// @ts-ignore
 import renderdemo from "../data/renderdemo.json";
 
 import Renderer from "./Renderer";
 
 describe("Renderer", () => {
-  it("will render without crashing", () => {
-    const { unmount, getByText } = render(() => (
+  it("matches snapshot", () => {
+    const { unmount, container } = render(() => (
       <Renderer data={JSON.stringify(renderdemo.data[0])} />
     ));
-    expect(getByText("Renderer Demo")).toBeInTheDocument();
-
+    expect(container).toMatchSnapshot();
+    unmount();
+  });
+  it("renders bold", () => {
+    const { unmount, container } = render(() => (
+      <Renderer data="this is some text, {@bold and now its bold} {@b with shorthand, too!}" />
+    ));
+    expect(container).toMatchInlineSnapshot();
     unmount();
   });
 });
