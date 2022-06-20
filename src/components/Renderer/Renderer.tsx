17835416bdba3934b08ace1b0cd7308c933bbb3c
Isaac <isaac@rubenfamily.com>
2022-06-20 13:41:31 -0700

    break tags out into file

--- src/components/Renderer/Renderer.tsx
+++ src/components/Renderer/Renderer.tsx
@@ -14,76 +14,13 @@ import {
 import { Dynamic } from "solid-js/web";
 import entryTypes from "./entryTypes";
 import parseData from "./parseData";
-
-import styles from "./Renderer.module.scss";
 import RenderError from "./RenderError";
+import { tagAlias, tagMap } from "./tags";
 import { Data, DataGroup, DataNode } from "./types";
 
+import styles from "./Renderer.module.scss";
+
 const cleanText = (text: string) => text.replaceAll("&quot;", '"');
-
-/**
- * maps alias shorthand "b" to full tag "bold"
- */
-const tagAlias = new Map([
-  ["b", "bold"],
-  ["i", "italic"],
-  ["u", "underline"],
-  ["s", "strike"],
-]);
-
-const pipe =
-  (Element: Component<{ p: string[]; p0?: string; p1?: string }>) =>
-  (props: { children: JSX.Element }): JSX.Element => {
-    const pipeSplit =
-      typeof props.children === "string" ? props.children.split("|") : [];
-    return <Element p={pipeSplit} p0={pipeSplit[0]} p1={pipeSplit[1]} />;
-  };
-
-const tag =
-  (
-    tagString: keyof JSX.IntrinsicElements,
-    obj?: {
-      class?: string;
-      style?: string;
-    }
-  ): Component<{ children: JSX.Element }> =>
-  (props) =>
-    (
-      <Dynamic
-        component={tagString}
-        children={props.children}
-        class={obj?.class}
-        style={obj?.style}
-      />
-    );
-
-const tagMap = new Map<string, Component<{ children: JSX.Element }>>(
-  Object.entries({
-    // intrinsic tags
-    bold: tag("b"),
-    italic: tag("i"),
-    strike: tag("s"),
-    code: tag("code"),
-    // styled intrinsic tags
-    underline: tag("span", { class: styles.underline }),
-    note: tag("i", { class: styles.note }),
-    // complex tags
-    color: pipe((props) => (
-      <span style={`color: #${props.p1}`}>{props.p0}</span>
-    )),
-    link: pipe((props) => (
-      <a href={props.p1 ?? props.p0} target="_blank" rel="noopener noreferrer">
-        {props.p0}
-      </a>
-    )),
-    filter: pipe((props) => (
-      <a title={props.p?.slice(1).join("|")} href="#">
-        {"UNSUPPORTED "}
-        {props.p0}
-      </a>
-    )),
-  })
-);
 
 /** cannot match nested tags */
 const tagMatcher = /(?<!$)(.*?)(?:{@(\w*)\s(.*?)}|$)/gm;
---
+++ src/components/Renderer/tags.tsx
@@ -0,0 +1,69 @@
+import { Component } from "solid-js";
+import { JSX } from "solid-js/jsx-runtime";
+import { Dynamic } from "solid-js/web";
+
+import styles from "./Renderer.module.scss";
+
+/**
+ * maps alias shorthand "b" to full tag "bold"
+ */
+export const tagAlias = new Map([
+  ["b", "bold"],
+  ["i", "italic"],
+  ["u", "underline"],
+  ["s", "strike"],
+]);
+
+const pipe =
+  (Element: Component<{ p: string[]; p0?: string; p1?: string }>) =>
+  (props: { children: JSX.Element }): JSX.Element => {
+    const pipeSplit =
+      typeof props.children === "string" ? props.children.split("|") : [];
+    return <Element p={pipeSplit} p0={pipeSplit[0]} p1={pipeSplit[1]} />;
+  };
+
+const tag =
+  (
+    tagString: keyof JSX.IntrinsicElements,
+    obj?: {
+      class?: string;
+      style?: string;
+    }
+  ): Component<{ children: JSX.Element }> =>
+  (props) =>
+    (
+      <Dynamic
+        component={tagString}
+        children={props.children}
+        class={obj?.class}
+        style={obj?.style}
+      />
+    );
+
+export const tagMap = new Map<string, Component<{ children: JSX.Element }>>(
+  Object.entries({
+    // intrinsic tags
+    bold: tag("b"),
+    italic: tag("i"),
+    strike: tag("s"),
+    code: tag("code"),
+    // styled intrinsic tags
+    underline: tag("span", { class: styles.underline }),
+    note: tag("i", { class: styles.note }),
+    // complex tags
+    color: pipe((props) => (
+      <span style={`color: #${props.p1}`}>{props.p0}</span>
+    )),
+    link: pipe((props) => (
+      <a href={props.p1 ?? props.p0} target="_blank" rel="noopener noreferrer">
+        {props.p0}
+      </a>
+    )),
+    filter: pipe((props) => (
+      <a title={props.p?.slice(1).join("|")} href="#">
+        {"UNSUPPORTED "}
+        {props.p0}
+      </a>
+    )),
+  })
+);
