7ee1b57b53a436b5d3a07e43146c557cd04f491f
Isaac <isaac@rubenfamily.com>
2022-06-21 00:08:57 -0700

    add new combinate function for objects

--- src/combinate.test.ts
+++ src/combinate.test.ts
@@ -1,23 +1,52 @@
 import { expect, it } from "vitest";
-import { arrayCombinate } from "./combinate";
+import { arrayCombinate, combinate } from "./combinate";
 
 it("returns all combinations of an array", () => {
   expect(arrayCombinate(["a", "b", "c", "d"])).toEqual([
-  [],
-  [ "a" ],
-  [ "b" ],
-  [ "a", "b" ],
-  [ "c" ],
-  [ "a", "c" ],
-  [ "b", "c" ],
-  [ "a", "b", "c" ],
-  [ "d" ],
-  [ "a", "d" ],
-  [ "b", "d" ],
-  [ "a", "b", "d" ],
-  [ "c", "d" ],
-  [ "a", "c", "d" ],
-  [ "b", "c", "d" ],
-  [ "a", "b", "c", "d" ]
-])
-})
+    [],
+    ["a"],
+    ["b"],
+    ["a", "b"],
+    ["c"],
+    ["a", "c"],
+    ["b", "c"],
+    ["a", "b", "c"],
+    ["d"],
+    ["a", "d"],
+    ["b", "d"],
+    ["a", "b", "d"],
+    ["c", "d"],
+    ["a", "c", "d"],
+    ["b", "c", "d"],
+    ["a", "b", "c", "d"],
+  ]);
+});
+
+it("returns all combinations of an object", () => {
+  expect(
+    combinate(["a", "b"], { a: 1, b: 2, c: 3, d: 4 })
+  ).toMatchInlineSnapshot(`
+    [
+      {
+        "c": 3,
+        "d": 4,
+      },
+      {
+        "a": 1,
+        "c": 3,
+        "d": 4,
+      },
+      {
+        "b": 2,
+        "c": 3,
+        "d": 4,
+      },
+      {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": 4,
+      },
+    ]
+  `);
+});
--- src/combinate.ts
+++ src/combinate.ts
@@ -3,26 +3,50 @@
 /**
  * returns an array with every combination of the keys of the passed array - not every ordering is returned
  */
-export const arrayCombinate = <T extends string | number>(array: T[]): Array<Array<T>> => {
-  const resultStack: Array<Array<T>> = [[]]
+export const arrayCombinate = <T extends string | number>(
+  array: T[]
+): Array<Array<T>> => {
+  const resultStack: Array<Array<T>> = [[]];
 
-  const len = Math.pow(2, array.length)
-  for(let i = 0; i < len; i++) {
-    const newArray: T[] = []
-    for(let j = 0; j < array.length; j++) {
-      if(i & Math.pow(2, j)) {
-        newArray.push(array[j])
+  const len = Math.pow(2, array.length);
+  for (let i = 0; i < len; i++) {
+    const newArray: T[] = [];
+    for (let j = 0; j < array.length; j++) {
+      if (i & Math.pow(2, j)) {
+        newArray.push(array[j]);
       }
     }
-    if(newArray.length !== 0) resultStack.push(newArray)
+    if (newArray.length !== 0) resultStack.push(newArray);
   }
 
   return resultStack;
-}
+};
 
-export const combinate = <T extends object>(keys: Array<keyof T & string>, object: T): T => {
-  const keyCombinations = arrayCombinate(keys)
-  return object
-}
+/* returns an array containing every object combination of keys*/
+export const combinate = <T extends object>(
+  keys: Array<keyof T & string>,
+  object: T
+): Array<T> => {
+  const resultStack: Array<T> = [];
 
-combinate(["a", "b"], {a: 2})
+  // object with the keys not present in keys array
+  const baseObject = Object.keys(object).reduce((acc, key) => {
+    if (!keys.includes(key)) {
+      acc[key] = object[key];
+    }
+    return acc;
+  }, {} as T);
+
+  const len = Math.pow(2, keys.length);
+  for (let i = 0; i < len; i++) {
+    const newObject = { ...baseObject };
+    for (let j = 0; j < keys.length; j++) {
+      if (i & Math.pow(2, j)) {
+        newObject[keys[j]] = object[keys[j]];
+      }
+    }
+    if (Object.keys(newObject).length !== 0) resultStack.push(newObject);
+  }
+
+  return resultStack;
+};
