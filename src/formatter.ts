7dbbba770e644a71bad32fe884e86adbdba80c5f
Isaac <isaac@rubenfamily.com>
2022-06-30 16:08:36 -0700

    capitalize output

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,8 +1,12 @@
 import { DataSpell } from "./components/Renderer/types";
 
+const capitalize = (s: string) => s[0].toUpperCase() + s.slice(1);
+
 const baseRanges = new Set(["self", ""]);
 export const fmtRange = (data: DataSpell["range"]) => {
-  if (data.distance === undefined) return data.type;
-  if (data.distance.amount === undefined) return data.distance.type;
-  return `${data.type} ${data.distance.type} ${data.distance.amount}`;
+  if (data.distance === undefined) return capitalize(data.type);
+  if (data.distance.amount === undefined) return capitalize(data.distance.type);
+  return capitalize(
+    `${data.type} ${data.distance.type} ${data.distance.amount}`
+  );
 };
