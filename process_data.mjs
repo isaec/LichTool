bb604a0f40aecaa54bfb24e95f26665f80a190d6
Isaac <isaac@rubenfamily.com>
2022-06-29 17:02:42 -0700

    process spells into processed_data dir

--- process_data.mjs
+++ process_data.mjs
@@ -1,4 +1,30 @@
 import glob from "glob-promise";
+import path from "path";
+import { promises as fs } from "fs";
 
-const result = await glob("spells/spells-*.json", { cwd: "data" });
-console.log(result);
+/** make globs nicer to write for data dir */
+const dataGlob = async (globStr) =>
+  (await glob(globStr, { cwd: "data" })).map((partialPath) =>
+    path.join("data", partialPath)
+  );
+
+// ensure out dir exists
+await fs.mkdir("processed_data");
+
+// copy spells over
+const spells = await dataGlob("spells/spells-*.json");
+const processedSpells = [];
+await Promise.all(
+  spells.map(async (spellPath) => {
+    const spellFileData = JSON.parse(
+      await fs.readFile(spellPath, "utf8")
+    ).spell;
+    spellFileData.forEach((spell) => {
+      if (spell.srd === true) processedSpells.push(spell);
+    });
+  })
+);
+await fs.writeFile(
+  "processed_data/spells.json",
+  JSON.stringify({ spell: processedSpells })
+);
