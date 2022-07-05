18a45a8a9224aa22fbde9e0175c6006a40eee534
Isaac <isaac@rubenfamily.com>
2022-07-05 10:49:34 -0700

    reroute naked view to search

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "solid-app-router";
+import { Navigate, Route, Routes } from "solid-app-router";
 import { Component } from "solid-js";
 
 import { RenderDemo, Search, Root } from "@routes";
@@ -9,6 +9,7 @@ const App: Component = () => (
     <Route path="/" component={Root} />
     <Route path="/renderdemo" component={RenderDemo} />
     <Route path="/search" component={Search} />
+    <Route path="/view" component={() => <Navigate href="/search" />} />
     <Route path="/view/:id" component={View} />
   </Routes>
 );
