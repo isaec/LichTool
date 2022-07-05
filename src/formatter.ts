3c9ff13dc71987ad608ec1ddc764a30fd597ec66
Isaac <isaac@rubenfamily.com>
2022-07-04 22:16:05 -0700

    handle radiuses

--- src/formatter.ts
+++ src/formatter.ts
@@ -2,7 +2,6 @@ import { DataSpell } from "./components/Renderer/types";
 
 const capitalize = (s: string) => s[0].toUpperCase() + s.slice(1);
 
-const baseRanges = new Set(["self", ""]);
 export const fmtRange = (data: DataSpell["range"]) => {
   if (data.distance === undefined) return capitalize(data.type);
   if (data.distance.amount === undefined) return capitalize(data.distance.type);
@@ -12,6 +11,10 @@ export const fmtRange = (data: DataSpell["range"]) => {
       if (data.distance.type === "miles" && data.distance.amount === 1)
         return "1 mile";
       return capitalize(`${data.distance.amount} ${data.distance.type}`);
+    case "radius":
+      return capitalize(
+        `Self (${data.distance.amount}-${data.distance.type} ${data.type})`
+      );
     default:
       return capitalize(
         `${data.type} ${data.distance.type} ${data.distance.amount}`
