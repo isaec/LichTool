162533cee01ef2a54515d276e36451f8bb666168
Isaac <isaac@rubenfamily.com>
2022-07-10 10:16:03 -0700

    add toaster

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import { render } from "solid-js/web";
 import "./index.scss";
 import App from "./App";
 import { Router } from "solid-app-router";
+import { Toaster } from "solid-toast";
 
 render(
   () => (
     <Router>
       <App />
+      <Toaster />
     </Router>
   ),
   document.getElementById("root") as HTMLElement
