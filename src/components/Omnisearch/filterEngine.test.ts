371c87fbed320556469dbec765270724757ea2e7
Isaac <isaac@rubenfamily.com>
2022-07-10 22:20:34 -0700

    more tests for filter parsing

--- src/components/Omnisearch/filterEngine.test.ts
+++ src/components/Omnisearch/filterEngine.test.ts
@@ -21,13 +21,35 @@ describe("parseFilter", () => {
         ["false", false],
       ],
     ],
+    [
+      "string",
+      [
+        ["fireball", "fireball"],
+        ["fire blt", "fire blt"],
+        ["123!", "123!"],
+        ["5.5", "5.5"],
+        ["/f", "/f"],
+        [
+          "/this looks like regex/ but its not!",
+          "/this looks like regex/ but its not!",
+        ],
+      ],
+    ],
+    [
+      "RegExp",
+      [
+        ["/f/", /f/],
+        ["/^f/", /^f/],
+        ["/something/i", /something/i],
+      ],
+    ],
   ] as Array<[string, Array<[string, ReturnType<typeof parseFilter>]>]>)(
     "parses %s into the correct literal",
     (type, cases) => {
       it.each(cases)(
         `parses %s into ${type} literal of "%s"`,
         (string, literal) => {
-          expect(parseFilter(string)).toBe(literal);
+          expect(parseFilter(string)).toStrictEqual(literal);
         }
       );
     }
