5d325da329f9ecca8347b182f8dcc3452740d14a
Isaac <isaac@rubenfamily.com>
2022-07-11 09:33:57 -0700

    filternengine unit tests

--- src/components/Omnisearch/filterEngine.test.ts
+++ src/components/Omnisearch/filterEngine.test.ts
@@ -1,8 +1,62 @@
+import { dataArray } from "@src/dataLookup";
 import { describe, expect, it } from "vitest";
-import { parseFilter } from "./filterEngine";
+import dataMiniSearch from "./dataMiniSearch";
+import { executeFilters, parseFilter, PopulatedFilter } from "./filterEngine";
 
 describe("filterEngine", () => {
-  it.todo("matches snapshot for search results given filter");
+  const tr = (arr: {}[]) => arr.map((o) => ({ ...o, use: true }));
+  const cases = [
+    {
+      query: "fireball",
+      filters: tr([{ key: "name", value: "fireball" }]),
+    },
+    {
+      filters: tr([
+        {
+          key: "name",
+          value: `/mind\\b/i`,
+        },
+      ]),
+    },
+    {
+      query: "m",
+      filters: tr([
+        {
+          key: "level",
+          value: "3",
+        },
+        {
+          key: "name",
+          value: "i",
+        },
+      ]),
+    },
+  ] as {
+    query: string;
+    filters: Array<PopulatedFilter>;
+  }[];
+
+  it.each(
+    cases.map((c): [string, string, PopulatedFilter[]] => [
+      c.query,
+      JSON.stringify(c.filters),
+      c.filters,
+    ])
+  )(
+    "matches snapshot for search results given query %s, filter %s",
+    (query, _filters, filters) => {
+      if (query === undefined) {
+        console.log(filters);
+        expect(dataArray.filter(executeFilters(filters)!)).toMatchSnapshot();
+      } else {
+        expect(
+          dataMiniSearch.search(query, {
+            filter: executeFilters(filters),
+          })
+        ).toMatchSnapshot();
+      }
+    }
+  );
 });
 
 describe("parseFilter", () => {
