e1687c6f092c48abcf6e998ffc9e257ef7fbbf67
Isaac <isaac@rubenfamily.com>
2022-07-14 13:45:04 -0700

    continue improving unit tests

--- src/components/Renderer/DataElements/DataSpellElement.test.tsx
+++ src/components/Renderer/DataElements/DataSpellElement.test.tsx
@@ -23,90 +23,6 @@ vi.mock("solid-app-router", () => ({
 }));
 
 describe("DataSpellElement", () => {
-  describe.each([
-    [
-      "exhibits general behavior",
-      generate<RawDataSpell>({
-        name: "spell name",
-        source: one(["PHB", "XGE"]),
-        page: 219,
-        level: 3,
-        school: "I",
-        time: [
-          {
-            number: 1,
-            unit: "action",
-          },
-        ],
-        range: {
-          type: "point",
-          distance: {
-            type: "self",
-          },
-        },
-        components: {
-          v: true,
-        },
-        duration: [
-          {
-            type: "timed",
-            duration: {
-              type: "minute",
-              amount: 1,
-            },
-            concentration: true,
-          },
-        ],
-        entries: ["Entry text, {@b bold!}"],
-      }),
-    ],
-    [
-      "displays text for levels",
-      generate<RawDataSpell>({
-        name: "spell name",
-        source: "PHB",
-        page: 100,
-        level: one([1, 2, 3, 4, 5]),
-        school: "P",
-        time: [
-          {
-            unit: "action",
-            number: 1,
-          },
-        ],
-        range: {
-          type: "point",
-          distance: {
-            type: "self",
-          },
-        },
-        components: {
-          v: true,
-        },
-        duration: [
-          {
-            type: "timed",
-            duration: {
-              type: "minute",
-              amount: 1,
-            },
-            concentration: false,
-          },
-        ],
-        entries: ["lol"],
-      }),
-    ],
-  ])(`DataSpellElement %s`, (description, data) => {
-    it.each(data)("matches snapshot for %s", (data) => {
-      const { unmount, container } = render(() => (
-        <DataSpellElement data={data} />
-      ));
-
-      expect(container).toMatchSnapshot();
-      unmount();
-    });
-  });
-
   describe.each(
     dataArray
       .filter(isDataSpell)
@@ -118,6 +34,22 @@ describe("DataSpellElement", () => {
       ));
 
       expect(container).toMatchSnapshot();
+      unmount();
+    });
+    it(`never renders with html containing "undefined"`, () => {
+      const { unmount, container, queryByText } = render(() => (
+        <DataSpellElement data={spell} />
+      ));
+
+      expect(queryByText(/undefined/i)).toBeNull();
+      unmount();
+    });
+    it(`never renders with html containing "[object Object]"`, () => {
+      const { unmount, container, queryByText } = render(() => (
+        <DataSpellElement data={spell} />
+      ));
+
+      expect(queryByText(/\[object Object\]/)).toBeNull();
       unmount();
     });
   });
