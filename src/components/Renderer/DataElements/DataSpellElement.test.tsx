b54e876d365af23be333d5ddfb8c7b8bd87becc7
Isaac <isaac@rubenfamily.com>
2022-07-14 13:36:42 -0700

    add more unit tests for data elements

--- src/components/Renderer/DataElements/DataSpellElement.test.tsx
+++ src/components/Renderer/DataElements/DataSpellElement.test.tsx
@@ -2,7 +2,12 @@ import { generate, one } from "generate-combinations";
 import { render } from "solid-testing-library";
 import { describe, expect, it, vi } from "vitest";
 import DataSpellElement from "./DataSpellElement";
-import { DataSpell, RawDataSpell } from "@src/dataLookup";
+import {
+  dataArray,
+  DataSpell,
+  isDataSpell,
+  RawDataSpell,
+} from "@src/dataLookup";
 
 vi.mock("./DataElement.module.scss", () => ({
   default: new Proxy(new Object(), {
@@ -12,86 +17,108 @@ vi.mock("./DataElement.module.scss", () => ({
   }),
 }));
 
-describe.each([
-  [
-    "exhibits general behavior",
-    generate<RawDataSpell>({
-      name: "spell name",
-      source: one(["PHB", "XGE"]),
-      page: 219,
-      level: 3,
-      school: "I",
-      time: [
-        {
-          number: 1,
-          unit: "action",
-        },
-      ],
-      range: {
-        type: "point",
-        distance: {
-          type: "self",
-        },
-      },
-      components: {
-        v: true,
-      },
-      duration: [
-        {
-          type: "timed",
-          duration: {
-            type: "minute",
-            amount: 1,
-          },
-          concentration: true,
-        },
-      ],
-      entries: ["Entry text, {@b bold!}"],
-    }),
-  ],
-  [
-    "displays text for levels",
-    generate<RawDataSpell>({
-      name: "spell name",
-      source: "PHB",
-      page: 100,
-      level: one([1, 2, 3, 4, 5]),
-      school: "P",
-      time: [
-        {
-          unit: "action",
-          number: 1,
-        },
-      ],
-      range: {
-        type: "point",
-        distance: {
-          type: "self",
-        },
-      },
-      components: {
-        v: true,
-      },
-      duration: [
-        {
-          type: "timed",
-          duration: {
-            type: "minute",
-            amount: 1,
-          },
-          concentration: false,
-        },
-      ],
-      entries: ["lol"],
-    }),
-  ],
-])(`DataSpellElement %s`, (description, data) => {
-  it.each(data)("matches snapshot for %s", (data) => {
-    const { unmount, container } = render(() => (
-      <DataSpellElement data={data} />
-    ));
+vi.mock("solid-app-router", () => ({
+  ...import("solid-app-router"),
+  Link: (props: any) => <a href={props.href}>{props.children}</a>,
+}));
 
-    expect(container).toMatchSnapshot();
-    unmount();
+describe("DataSpellElement", () => {
+  describe.each([
+    [
+      "exhibits general behavior",
+      generate<RawDataSpell>({
+        name: "spell name",
+        source: one(["PHB", "XGE"]),
+        page: 219,
+        level: 3,
+        school: "I",
+        time: [
+          {
+            number: 1,
+            unit: "action",
+          },
+        ],
+        range: {
+          type: "point",
+          distance: {
+            type: "self",
+          },
+        },
+        components: {
+          v: true,
+        },
+        duration: [
+          {
+            type: "timed",
+            duration: {
+              type: "minute",
+              amount: 1,
+            },
+            concentration: true,
+          },
+        ],
+        entries: ["Entry text, {@b bold!}"],
+      }),
+    ],
+    [
+      "displays text for levels",
+      generate<RawDataSpell>({
+        name: "spell name",
+        source: "PHB",
+        page: 100,
+        level: one([1, 2, 3, 4, 5]),
+        school: "P",
+        time: [
+          {
+            unit: "action",
+            number: 1,
+          },
+        ],
+        range: {
+          type: "point",
+          distance: {
+            type: "self",
+          },
+        },
+        components: {
+          v: true,
+        },
+        duration: [
+          {
+            type: "timed",
+            duration: {
+              type: "minute",
+              amount: 1,
+            },
+            concentration: false,
+          },
+        ],
+        entries: ["lol"],
+      }),
+    ],
+  ])(`DataSpellElement %s`, (description, data) => {
+    it.each(data)("matches snapshot for %s", (data) => {
+      const { unmount, container } = render(() => (
+        <DataSpellElement data={data} />
+      ));
+
+      expect(container).toMatchSnapshot();
+      unmount();
+    });
+  });
+
+  describe.each(
+    dataArray
+      .filter(isDataSpell)
+      .map((data): [string, typeof data] => [data.name, data])
+  )(`%s`, (_name, spell) => {
+    it("matches snapshot", () => {
+      const { unmount, container } = render(() => (
+        <DataSpellElement data={spell} />
+      ));
+
+      expect(container).toMatchSnapshot();
+      unmount();
+    });
   });
 });
