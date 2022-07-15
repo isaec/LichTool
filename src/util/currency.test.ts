546004865ac0a7b3408d1e5039f771450136653c
Isaac <isaac@rubenfamily.com>
2022-07-15 16:23:03 -0700

    add convert floor function

--- src/util/currency.test.ts
+++ src/util/currency.test.ts
@@ -1,5 +1,5 @@
 import { Currency } from "@src/generalTypes";
-import { convert } from "./currency";
+import { convert, convertFloor } from "./currency";
 
 describe("convert", () => {
   it.each([
@@ -14,6 +14,23 @@ describe("convert", () => {
     "converts %s %s to %s",
     (value, from, to, expected) => {
       expect(convert(value, from, to)).toBe(expected);
+    }
+  );
+});
+
+describe("convertFloor", () => {
+  it.each([
+    [10, "cp", "sp", 1],
+    [1, "cp", "cp", 1],
+    [1, "cp", "sp", 0],
+    [1, "cp", "ep", 0],
+    [100, "cp", "gp", 1],
+    [100_000, "cp", "pp", 100],
+    [1e6, "pp", "cp", 1e9],
+  ] as Array<[number, Currency, Currency, number]>)(
+    "converts %s %s to %s",
+    (value, from, to, expected) => {
+      expect(convertFloor(value, from, to)).toBe(expected);
     }
   );
 });
--- src/util/currency.ts
+++ src/util/currency.ts
@@ -1,4 +1,27 @@
 import { Currency, currencyToCopperMap } from "@src/generalTypes";
 
+/**
+ *
+ * @param value the value to convert from
+ * @param from the currency of {@link value}
+ * @param to the currency to convert to
+ * @returns the new value in the {@link to} currency as a number
+ */
 export const convert = (value: number, from: Currency, to: Currency): number =>
   value * (currencyToCopperMap.get(from)! / currencyToCopperMap.get(to)!);
+
+/**
+ *
+ * @param value the value to convert from
+ * @param from the currency of {@link value}
+ * @param to the currency to convert to
+ * @returns the new value in the {@link to} currency as a ***floored*** number
+ */
+export const convertFloor = (
+  value: number,
+  from: Currency,
+  to: Currency
+): number =>
+  Math.floor(
+    value * (currencyToCopperMap.get(from)! / currencyToCopperMap.get(to)!)
+  );
