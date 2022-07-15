06b59db062fb1b37a2f59f3c5fa7a5ccfb892611
Isaac <isaac@rubenfamily.com>
2022-07-15 16:16:09 -0700

    more test cases

--- src/util/currency.test.ts
+++ src/util/currency.test.ts
@@ -5,6 +5,11 @@ describe("convert", () => {
   it.each([
     [10, "cp", "sp", 1],
     [1, "cp", "cp", 1],
+    [1, "cp", "sp", 0.1],
+    [1, "cp", "ep", 0.02],
+    [100, "cp", "gp", 1],
+    [100_000, "cp", "pp", 100],
+    [1e6, "pp", "cp", 1e9],
   ] as Array<[number, Currency, Currency, number]>)(
     "converts %s %s to %s",
     (value, from, to, expected) => {
