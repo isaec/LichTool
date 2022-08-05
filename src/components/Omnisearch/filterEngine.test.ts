8ba9e0c87161132733dc7623efce0f669ee75c44
Isaac <isaac@rubenfamily.com>
2022-08-05 15:04:20 -0700

    more unit tests

--- src/components/Omnisearch/filterEngine.test.ts
+++ src/components/Omnisearch/filterEngine.test.ts
@@ -170,6 +170,8 @@ describe("testFilter", () => {
   const fb = dataMap.get("spell_PHB-Fireball")!;
 
   it.each([
+    // strings
+
     // case insensitive
     [true, fb, f("name", "fireball")],
     [true, fb, f("name", "FIREBALL")],
@@ -180,12 +182,25 @@ describe("testFilter", () => {
     // regex is case sensitive
     [false, fb, f("name", "/FIREBALL/")],
     [true, fb, f("name", "/F.+b/")],
-    // numbers match if equal
-    [true, fb, f("level", "3")],
-    // numbers don't match if not equal
-    [false, fb, f("level", "4")],
     // non matching
     [false, fb, f("name", "aid")],
+
+    // numbers
+
+    // match if equal
+    [true, fb, f("level", "3")],
+    // don't match if not equal
+    [false, fb, f("level", "4")],
+    // no decimals (should this change in the future?)
+    [false, fb, f("level", "3.0")],
+    // regex (as a string)
+    [true, fb, f("level", "/3/")],
+
+    // booleans
+
+    [true, fb, f("hasFluffImages", "true")],
+    [false, fb, f("hasFluffImages", "t")],
+    [false, fb, f("hasFluffImages", "false")],
   ])("returns %s for object %s with filter %s", (expected, obj, filter) => {
     expect(testFilter(obj, filter)).toBe(expected);
   });
